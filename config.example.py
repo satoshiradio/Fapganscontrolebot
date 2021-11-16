6408c596873368911b1da74ef70e45a7a704038e
Rachid BM <rachidbm@github.com>
2021-11-16 21:47:23 +0100

    Mention configured admin users when a gans was sent when not allowed

--- config.example.py
+++ config.example.py
@@ -1,6 +1,10 @@
 class BotConfig:
     TOKEN = "1234567890:ABCDEFGH123_IJKLMNOPQRSTUVWXYZ456789"
     FAPGANS_STICKER_ID = "AgADXgADUomRIw"
+    # list of Telegram users that are the admins of this bot
+    ADMINS = {
+        'RawChid': '123'
+    }
 
 
 class DbConfig:
